d775ae710d78799c1a8f6596de71703adfd35f12
nick <nikoloz.danelia@redberry.ge>
2023-06-07 17:00:15 +0400

    feat: sanctum login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EmailVerificationController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Requests\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
@@ -21,5 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/email/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])->middleware(['signed'])->name('verification.verify');
-
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return $request->user();
+});
 Route::post('/register', [RegisterController::class, 'store']);
+Route::post('/login', [LoginController::class , 'authenticate']);
