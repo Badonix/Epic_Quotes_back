7510b10b9101ce8d91302986e97cbd9f49d86abc
nick <nikoloz.danelia@redberry.ge>
2023-07-20 11:20:36 +0400

    choir: down functions for genres

--- database/migrations/2023_07_18_144623_modify_genres_table.php
+++ database/migrations/2023_07_18_144623_modify_genres_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('genres', function (Blueprint $table) {
-            //
+            $table->string('name')->change();
         });
     }
 };
